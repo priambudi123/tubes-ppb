343c722beaac464972b15fc742265a20c517ba0f
priambudi123 <priambudib.31@gmail.com>
2023-12-31 12:03:51 +0700

    first commit

---
+++ lib/UI/profile/components/body.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Service/apiService.dart';
+import 'package:flutter_application_1/UI/login/login_screen.dart';
+import 'package:flutter_application_1/constants.dart';
+import 'package:iconsax/iconsax.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 20),
+          ProfileMenu(
+            icon: Icon(Iconsax.user),
+            text: "Edit Profile",
+            press: () {},
+          ),
+          ProfileMenu(
+            icon: Icon(Icons.settings),
+            text: "Settings",
+            press: () {},
+          ),
+          ProfileMenu(
+              icon: Icon(Icons.logout),
+              text: "Log out",
+              press: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Logout'),
+                        content: Text('Are you sure you want to logout?'),
+                        actions: <Widget>[
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.blueAccent,
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              'Cancel',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              APIService.logut();
+                              final snackBar = SnackBar(
+                                content: Text(
+                                  "Logout Success!",
+                                  style: TextStyle(
+                                    color: kPrimaryColor,
+                                  ),
+                                ),
+                                backgroundColor: kSecondaryLightColor,
+                                duration: Duration(seconds: 4),
+                                shape: StadiumBorder(),
+                                behavior: SnackBarBehavior.floating,
+                              );
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return LoginScreen();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Text('Logout'),
+                          ),
+                        ],
+                      );
+                    });
+              }),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu({
+    Key? key,
+    required this.text,
+    required this.icon,
+    required this.press,
+  }) : super(key: key);
+
+  final String text;
+  final Icon icon;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(20),
+          backgroundColor: Color(0xFFF5F6F9),
+        ),
+        onPressed: press,
+        child: Row(
+          children: [
+            icon,
+            SizedBox(width: 20),
+            Expanded(
+              child: Text(
+                text,
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ),
+            Icon(Icons.arrow_forward_ios, color: kSecondaryColor)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/UI/launcher/launcher.dart';
+import 'package:flutter_application_1/UI/main/main.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Latihan Flutter',
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme),
+      ),
+      home: LauncherPage(),
+    );
+  }
+}
